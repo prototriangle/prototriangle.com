c119575da5d785552737113fcd4c62adaf3d238b
prototriangle <alex@prototriangle.com>
2020-09-02 17:39:16 +0100

    Improve nav versitility

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,65 +1,72 @@
 <template>
     <ul class="nav-bar">
-        <NavItem v-for="(item, index) in menu" :key="index" :item-data="item"/>
+        <NavItem @navigate="navHandler" v-for="(item, index) in menu" :key="index" :item-data="item"/>
     </ul>
 </template>
 
 <script>
-    import NavItem from "@/components/NavItem";
+import NavItem from "@/components/NavItem";
 
-    export default {
-        name: 'NavBar',
-        props: {
-            menu: Array
-        },
-        components: {
-            NavItem
+export default {
+    name: 'NavBar',
+    props: {
+        menu: Array
+    },
+    components: {
+        NavItem
+    },
+    methods: {
+        navHandler: function (args) {
+            this.$emit("navigate", args);
         }
     }
+}
 </script>
 
 <style>
-    ul.nav-bar {
-        /*width: 100%;*/
-        font-size: 1.4rem;
-        font-weight: bold;
-        font-family: "Roboto Slab", serif;
-        list-style-type: none;
-        padding: 0;
-        display: flex;
-        flex-direction: row;
-        justify-content: right;
-    }
+ul.nav-bar {
+    /*width: 100%;*/
+    font-size: 1.4rem;
+    font-weight: bold;
+    font-family: "Roboto Slab", serif;
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+}
 
-    ul.nav-bar > li {
-        /*width: 50%;*/
-        display: block;
-        float: left;
-        /*text-align: center;*/
-    }
+ul.nav-bar > li {
+    /*width: 50%;*/
+    display: block;
+    float: left;
+    /*text-align: center;*/
+}
 
-    ul.nav-bar > li > a,
-    ul.nav-bar > li > a:visited {
-        /*border-left: 3px black solid;*/
-        /*border-right: 3px black solid;*/
-        /*padding-left: 1vw;*/
-        /*padding-right: 1vw;*/
-    }
+ul.nav-bar > li > a,
+ul.nav-bar > li > a:visited {
+    /*border-left: 3px black solid;*/
+    /*border-right: 3px black solid;*/
+    /*padding-left: 1vw;*/
+    /*padding-right: 1vw;*/
+}
 
-    ul.nav-bar li a,
-    ul.nav-bar li a:visited {
-        text-decoration: none;
-        color: black;
-        text-shadow: 0 0 2px white;
-        transition: all 0.4s;
-    }
+ul.nav-bar li a,
+ul.nav-bar li a:visited {
+    text-decoration: none;
+    color: black;
+    text-shadow: 0 0 2px white;
+    transition: all 0.4s;
+}
 
-    ul.nav-bar li a:hover,
-    ul.nav-bar li a:focus,
-    ul.nav-bar li a:active {
-        text-decoration: underline;
-        color: #f6ffff;
-        text-shadow: 0 3px 8px black;
-        transition: all 0.2s;
-    }
+ul.nav-bar li a:hover,
+ul.nav-bar li a:focus,
+ul.nav-bar li a:active,
+ul.nav-bar li a[triggered] {
+    text-decoration: underline;
+    color: #f6ffff;
+    text-shadow: 0 3px 8px black;
+    transition: all 0.2s;
+}
+
 </style>
--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li class="nav-item" @mouseleave="subClose">
-        <a :href="itemData.url" @mouseover="subOpen">{{itemData.text}}</a>
+        <a ref="text" :data-scroll="itemData.url[0] === '#'" :href="itemData.url" @click="notifyNavigate(itemData.url)" @mouseover="subOpen" @touchstart="handleTouch">{{ itemData.text }}</a>
         <div v-if="itemData.submenu !== undefined" :class="{hidden: !open}" class="sub-nav">
             <ul>
                 <NavItem v-for="(item, index) in itemData.submenu" :key="index" :item-data="item"/>
@@ -10,41 +10,61 @@
 </template>
 
 <script>
-    export default {
-        name: 'NavItem',
-        props: {
-            itemData: Object
+export default {
+    name: 'NavItem',
+    props: {
+        itemData: Object
+    },
+    methods: {
+        subOpen: function () {
+            this.open = true;
         },
-        methods: {
-            subOpen: function () {
-                this.open = true;
-            },
-            subClose: function () {
-                this.open = false;
-            }
+        subClose: function () {
+            this.open = false;
         },
-        data: function () {
-            return {
-                open: false
-            }
+        handleTouch: function () {
+            if (this.itemData.submenu === undefined) return;
+            // console.log("Touch Handle");
+            this.open = !this.open;
+            this.$el.firstElementChild.toggleAttribute("triggered");
+        },
+        notifyNavigate: function (target) {
+            this.$emit("navigate", target);
+        }
+    },
+    data: function () {
+        return {
+            open: false
         }
     }
+}
 </script>
 
 <style>
-    .sub-nav ul {
-        list-style-type: none;
-        float: none;
-        padding: 0;
-    }
+.sub-nav ul {
+    list-style-type: none;
+    float: none;
+    padding: 0;
+}
 
-    .sub-nav ul li {
-        display: block;
-        float: none;
-        padding-top: 1vw;
-    }
+.sub-nav ul li {
+    display: block;
+    float: none;
+    padding-top: 1vw;
+    //pointer-events: none;
+}
 
-    .sub-nav.hidden {
-        opacity: 0;
-    }
+.sub-nav {
+    cursor: unset;
+}
+
+.sub-nav.hidden {
+    opacity: 0;
+}
+
+
+.sub-nav.hidden ul li > a {
+    pointer-events: none;
+}
+
 </style>
