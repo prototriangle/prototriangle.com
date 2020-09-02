b60bfd9f53afad96fe1dc32e38fd9e041e674ea4
prototriangle <alex@prototriangle.com>
2020-09-02 17:35:56 +0100

    Improve logo ik component

--- src/components/LogoIK.vue
+++ src/components/LogoIK.vue
@@ -6,12 +6,12 @@
             xmlns="http://www.w3.org/2000/svg"
             :id="idString"
             version="1.1"
-            viewBox="-260 -180 500 750"
-            height="500"
-            width="700">
+            :viewBox="'0 0 ' + svgWidth + ' ' + svgHeight"
+            :height="svgHeight"
+            :width="svgWidth">
         <svg:style id="base-style">
             svg {
-            stroke-width:10;
+            stroke-width: {{ strokeWidth }};
             stroke-linecap:round;
             stroke-linejoin:round;
             stroke-miterlimit:4;
@@ -21,34 +21,41 @@
             #link-layer, #joint-layer {
             fill:none;
             fill-opacity:0;
-            stroke:#000000;
-            stroke-opacity:1
+            stroke:currentColor;
+            stroke-opacity:1;
             }
 
-            #joint-mask #white-path {
+            #joint-mask #mask-geom {
+            fill:none;
+            fill-opacity:0;
+            }
+
+            #mask-geom #white-path {
             fill:none;
             fill-opacity:0;
             stroke:#ffffff;
-            stroke-opacity:1
+            stroke-opacity:1;
+            stroke-width: {{ strokeWidth }};
             }
 
-            #joint-mask black-circles {
+            #mask-geom #black-circles {
             fill:#000000;
             fill-opacity:1;
             stroke:#000000;
-            stroke-opacity:1
+            stroke-opacity:1;
+            stroke-width: 1;
             }
         </svg:style>
         <svg:defs id="defs">
             <svg:filter id="inset-shadow">
                 <!-- Shadow Offset -->
-                <svg:feOffset dx="10" dy="10"/>
+                <svg:feOffset dx="6" dy="6"/>
                 <!-- Shadow Blur -->
-                <svg:feGaussianBlur stdDeviation="10" result="offset-blur"/>
+                <svg:feGaussianBlur stdDeviation="8" result="offset-blur"/>
                 <!-- Invert the drop shadow to create an inner shadow -->
                 <svg:feComposite operator="out" in="SourceGraphic" in2="offset-blur" result="inverse"/>
                 <!-- Color & Opacity -->
-                <svg:feFlood flood-color="black" flood-opacity="1" result="color"/>
+                <svg:feFlood flood-color="currentColor" flood-opacity="0.9" result="color"/>
                 <!-- Clip color inside shadow -->
                 <svg:feComposite operator="in" in="color" in2="inverse" result="shadow"/>
                 <!-- Shadow Opacity -->
@@ -57,18 +64,21 @@
                     <svg:feFuncA type="linear" slope="1.1"/>
                 </svg:feComponentTransfer>
             </svg:filter>
-            <svg:g id="joint-circles">
+            <svg:g id="joint-circles" shape-rendering="geometricPrecision">
                 <svg:circle v-for="(j, i) in points.slice(0, -1)" :id="'c' + (i+1)" :key="i" :cx="j.x" :cy="j.y"
-                            r="20"/>
-                <svg:circle id="c4" :cx="ee.x" :cy="ee.y" r="20" stroke-dasharray="92"
+                            :r="circleRadius"/>
+                <svg:circle id="c4" :cx="ee.x" :cy="ee.y" :r="circleRadius" stroke-dasharray="92"
                             :transform="'rotate(' + eeAngle + ', ' + ee.x + ', ' + ee.y +')'"/>
             </svg:g>
             <svg:polyline id="arm-path" :points="pointsString"/>
+            <svg:g id="mask-geom">
+                <svg:use id="white-path" x="0" y="0" xlink:href="#arm-path"/>
+                <svg:use id="black-circles" x="0" y="0" xlink:href="#joint-circles"/>
+            </svg:g>
+            <svg:mask id="joint-mask">
+                <svg:use x="0" y="0" xlink:href="#mask-geom"/>
+            </svg:mask>
         </svg:defs>
-        <svg:mask id="joint-mask">
-            <svg:use id="white-path" x="0" y="0" xlink:href="#arm-path"/>
-            <svg:use id="black-circles" x="0" y="0" xlink:href="#joint-circles"/>
-        </svg:mask>
         <svg:g filter="url(#inset-shadow)">
             <svg:g id="link-layer" mask="url(#joint-mask)">
                 <svg:use x="0" y="0" xlink:href="#arm-path"/>
@@ -81,102 +91,133 @@
 </template>
 
 <script>
-    import {Solver} from "@/ultils/Solver";
+import {Solver} from "@/ultils/Solver";
 
-    function calcAngleDegrees(x, y) {
-        return Math.atan2(y, x) * 180 / Math.PI;
-    }
+function calcAngleDegrees(x, y) {
+    return Math.atan2(y, x) * 180 / Math.PI;
+}
 
-    let initPoints = [
-        {x: 0, y: 0},
-        {x: 55, y: -71},
-        {x: 110, y: -11},
-        {x: 170, y: -56,}
-    ]
+let initPoints = [
+    {x: 0, y: 0},
+    {x: 55 * 2, y: -71 * 2},
+    {x: 110 * 2, y: -11 * 2},
+    {x: 170 * 2, y: -56 * 2}
+]
 
-    let solver = new Solver(initPoints)
+let solver = new Solver(initPoints)
 
-    export default {
-        name: 'LogoIK',
-        props: {
-            clickEvent: Object,
-            origin: Object,
-            idString: {
-                type: String,
-                default: () => "logo"
-            }
-        }, watch: {
-            origin: function (rawO) {
-                const o = this.coordConvert(rawO);
-                // console.log("New Origin");
-                console.log(o);
-                const d = solver.diffV(this.points[0], o);
-                const f = v => {
-                    return {x: v.x + d.x, y: v.y + d.y}
-                };
-                initPoints = initPoints.map(f);
-                this.points = this.points.map(f);
-                solver.points = this.points;
-            },
-            clickEvent: function (evt) {
-                if (!evt.valid) return;
-                let svgCoords = this.coordConvert(evt);
-                if (svgCoords === null) return;
-                let goal = {x: svgCoords.x, y: svgCoords.y};
-                this.countdown = 50;
-                this.tweenVector = solver.mul(1 / 50, solver.diffV(this.ee, goal));
-                this.targetPoint = goal;
-                this.timers.push(window.setInterval(this.runLoop, 20));
-            }
-
+export default {
+    name: 'LogoIK',
+    props: {
+        doIK: Boolean,
+        clickEvent: Object,
+        origin: Object,
+        idString: {
+            type: String,
+            default: () => "logo"
         },
-        computed: {
-            ee: function () {
-                return this.points[this.points.length - 1]
-            },
-            pointsString: function () {
-                let arr = []
-                this.points.forEach((j) => {
-                    arr.push(j.x.toString() + ',' + j.y.toString())
-                });
-                return arr.join(' ');
+        svgWidth: Number,
+        svgHeight: Number,
+    }, watch: {
+        origin: function (rawO) {
+            if (!this.doIK || !rawO) return;
+            const o = this.coordConvert(rawO);
+            // console.log("New Origin");
+            // console.log(o);
+            const d = solver.diffV(this.points[0], o);
+            if (!d)
+                return null;
+            const f = v => {
+                return {x: v.x + d.x, y: v.y + d.y}
+            };
+            initPoints = initPoints.map(f);
+            this.points = this.points.map(f);
+            solver.points = this.points;
+        },
+        clickEvent: function (evt) {
+            if (!this.doIK || !evt.valid) return;
+            let svgCoords = this.coordConvert(evt);
+            if (svgCoords === null) return;
+            this.startPoint = this.coordConvert(this.ee);
+            console.log(`new start point (${this.startPoint.x},${this.startPoint.y})`);
+            this.targetPoint = {x: svgCoords.x, y: svgCoords.y};
+            console.log(`new target point (${this.targetPoint.x},${this.targetPoint.y})`);
+            this.animateStart = undefined;
+            if (!this.isAnimating)
+                window.requestAnimationFrame(this.runLoop);
+            this.$el.style.color = '#a1430a';
+        }
+
+    },
+    computed: {
+        ee: function () {
+            return this.points[this.points.length - 1]
+        },
+        pointsString: function () {
+            let arr = []
+            this.points.forEach((j) => {
+                arr.push(j.x.toString() + ',' + j.y.toString())
+            });
+            return arr.join(' ');
+        },
+        bufferAmount: function () {
+            return this.circleRadius * 1.6;
+        }
+    },
+    methods: {
+        runLoop(timestamp) {
+            if (this.animateStart === undefined) {
+                this.animateStart = timestamp;
             }
-        },
-        created() {
-            window.setTimeout(() => this.svg = document.getElementById(this.idString), 20);
-        },
-        methods: {
-            runLoop() {
-                // console.log(this.countdown);
-                if (this.countdown <= 0) this.timers.forEach(t => window.clearInterval(t));
-                this.tweenPoint = solver.add(this.ee, this.tweenVector);
-                // console.log(JSON.stringify(this.tweenPoint));
-                this.points = solver.runIK(this.tweenPoint);
+            const progress = (timestamp - this.animateStart) / this.animateTime;
+            const progBarWidth = 80;
+            const dispProg = Math.min(progBarWidth, Math.floor(progress * progBarWidth));
+            console.log(`${"▰".repeat(dispProg)}${"▱".repeat(progBarWidth - dispProg)}`);
+            if (progress >= 1) {
+                this.$el.style.color = 'black';
+                this.animateStart = undefined;
+                this.startPoint = this.ee;
+                this.isAnimating = false;
+            } else {
+                this.tweenPoint = solver.add(solver.mul(progress, this.targetPoint), solver.mul(1 - progress, this.startPoint));
+                this.points = solver.runIK(this.tweenPoint, 8);
                 const penJoint = this.points[this.points.length - 2];
-                this.eeAngle = calcAngleDegrees(this.ee.x - penJoint.x, this.ee.y - penJoint.y) + 45;
-                this.countdown -= 1;
-            },
-            coordConvert: function (screenCoords) {
-                let pt = this.svg.createSVGPoint();
-                pt.x = screenCoords.x;
-                pt.y = screenCoords.y;
-                return pt.matrixTransform(this.svg.getScreenCTM().inverse());
+                this.eeAngle = calcAngleDegrees(this.ee.x - penJoint.x, this.ee.y - penJoint.y) + 160;
+                this.isAnimating = true;
+                window.requestAnimationFrame(this.runLoop);
             }
         },
-        data: function () {
-            return {
-                eeAngle: 6,
-                targetPoint: initPoints[initPoints.length - 1],
-                tweenPoint: initPoints[initPoints.length - 1],
-                tweenVector: {x: 0, y: 0},
-                points: initPoints,
-                countdown: 0,
-                timers: [],
-                threshold: 100,
-                svg: null
-            }
+        coordConvert: function (screenCoords) {
+            // noinspection JSUnresolvedFunction
+            let pt = this.$el.createSVGPoint();
+            pt.x = screenCoords.x;
+            pt.y = screenCoords.y;
+            // noinspection JSUnresolvedFunction
+            const ctm = this.$el.getScreenCTM();
+            if (!ctm)
+                return null;
+            const ret = pt.matrixTransform(ctm.inverse());
+            ret.x = Math.max(Math.min(ret.x, this.svgWidth - this.bufferAmount), this.bufferAmount);
+            ret.y = Math.max(Math.min(ret.y, this.svgHeight - this.bufferAmount), this.bufferAmount);
+            return ret;
+        }
+    },
+    data: function () {
+        return {
+            eeAngle: 125,
+            targetPoint: initPoints[initPoints.length - 1],
+            startPoint: initPoints[initPoints.length - 1],
+            tweenPoint: initPoints[initPoints.length - 1],
+            points: initPoints,
+            animateTime: 2000,
+            animateStart: undefined,
+            threshold: 100,
+            circleRadius: 40,
+            strokeWidth: 20,
+            isAnimating: false,
         }
     }
+}
 </script>
 
 <style>
